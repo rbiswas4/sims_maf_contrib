77f488338f66946ca251177a665d4497210153e7
R. Biswas <rbiswas4@gmail.com>
2015-09-07 11:28:31 -0700

    removed direction of stdout and  stderr to /dev/null

--- scripts/sims_maf_setup.sh
+++ scripts/sims_maf_setup.sh
@@ -2,7 +2,8 @@
 set -e 
 conda config --add channels http://eupsforge.net/conda/dev
 conda config --set always_yes true
-conda install lsst-sims-maf > /dev/null 2>&1
+conda install lsst-sims-maf
+# > /dev/null 2>&1
 
 source eups-setups.sh
 setup sims_maf
