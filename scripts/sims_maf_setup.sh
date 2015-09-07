a4ff67fd7002f86c9c8cee8063b50fbc13410d1d
R. Biswas <rbiswas4@gmail.com>
2015-09-06 23:16:31 -0700

    set conda config to assume yes as answers to all questions

--- scripts/sims_maf_setup.sh
+++ scripts/sims_maf_setup.sh
@@ -1,7 +1,8 @@
 # #!/usr/bin/env bash
 set -e 
 conda config --add channels http://eupsforge.net/conda/dev
-conda install lsst-sims-maf
+conda config --set always_yes true
+conda install lsst-sims-maf > /dev/null 2>&1
 
 source eups-setups.sh
 setup sims_maf
