127cc82e117094526e59246a3b75f03224b74e20
R. Biswas <rbiswas4@gmail.com>
2015-09-07 15:00:49 -0700

    Checking disk space before sims_maf_install

--- scripts/sims_maf_setup.sh
+++ scripts/sims_maf_setup.sh
@@ -3,7 +3,10 @@ set -e
 conda config --add channels http://eupsforge.net/conda/dev
 conda config --set always_yes true
 echo $PWD
+echo 'checking disk space availability'
+du -kh . 
 echo 'installing sims-maf'
+
 conda install lsst-sims-maf > /dev/null 2>&1
 
 echo 'done installing sims-maf'
