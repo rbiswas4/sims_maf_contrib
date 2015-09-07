5233ddace5b1a47c8c6e3179c95ebdc080082147
R. Biswas <rbiswas4@gmail.com>
2015-09-07 12:43:11 -0700

    Changed travis.yml and script to echo data directories better

--- scripts/sims_maf_data.sh
+++ scripts/sims_maf_data.sh
@@ -1,12 +1,16 @@
 # #!/usr/bin/env bash
 set -e 
+echo 'Make data directory'
+echo $PWD
 mkdir ../data
 cd ../data
+echo 'The data directory is'
 echo $PWD
-curl -O http://www.astro.washington.edu/users/lynnej/opsim/ops2_1114_sqlite.db
-curl -O http://www.astro.washington.edu/users/lynnej/opsim/enigma_1189_sqlite.db.gz
-gunzip *.gz
-cd ../science/Transients
+curl -O http://www.astro.washington.edu/users/lynnej/opsim/ops2_1114_sqlite.db > /dev/null 2>&1
+curl -O http://www.astro.washington.edu/users/lynnej/opsim/enigma_1189_sqlite.db.gz > /dev/null 2>&1
+gunzip *.gz > /dev/null 2>&1
+cd ../science/Transients/
+echo $PWD
 ln -s ../data/*.db
 
 
