0b40fbb7b1b3c3218b986c7241e8e7e449f8be23
R. Biswas <rbiswas4@gmail.com>
2015-09-07 13:05:52 -0700

    tried to fix linking issues

--- scripts/sims_maf_data.sh
+++ scripts/sims_maf_data.sh
@@ -2,15 +2,11 @@
 set -e 
 echo 'Make data directory'
 echo $PWD
-mkdir ../data
-cd ../data
-echo 'The data directory is'
-echo $PWD
+mkdir -p ./data
 curl -O http://www.astro.washington.edu/users/lynnej/opsim/ops2_1114_sqlite.db > /dev/null 2>&1
 curl -O http://www.astro.washington.edu/users/lynnej/opsim/enigma_1189_sqlite.db.gz > /dev/null 2>&1
-gunzip *.gz > /dev/null 2>&1
-cd ../science/Transients/
-echo $PWD
-ln -s ../data/*.db
+gunzip *.gz
+for dbfile in `ls *.db`; do echo $dbfile; do mv $dbfile ./data; do ln -s ./science/Transients/$dbfile ./data/$dbfile; done
+rm *.gz
 
 
