5dd07780a3d9b4634238e4403850133ffae0338f
R. Biswas <rbiswas4@gmail.com>
2015-09-18 09:48:39 -0700

    Changed zipping options to prevent having zip and unzipped file simultaneously to save disk space which was causing a crash

--- scripts/sims_maf_data.sh
+++ scripts/sims_maf_data.sh
@@ -3,11 +3,14 @@ set -e
 echo 'Make data directory'
 echo $PWD
 mkdir -p ./data
-curl -O http://www.astro.washington.edu/users/lynnej/opsim/ops2_1114_sqlite.db > /dev/null 2>&1
-curl -O http://www.astro.washington.edu/users/lynnej/opsim/enigma_1189_sqlite.db.gz > /dev/null 2>&1
+wget -nc -q http://www.astro.washington.edu/users/lynnej/opsim/ops2_1114_sqlite.db
+#http://ops2.tuc.noao.edu/runs/ops2_1114/data/ops2_1114_sqlite.db
+wget -q -O - http://www.astro.washington.edu/users/lynnej/opsim/enigma_1189_sqlite.db.gz | gunzip -c > enigma_1189_sqlite.db
+# curl -O http://www.astro.washington.edu/users/lynnej/opsim/ops2_1114_sqlite.db > /dev/null 2>&1
+#curl -O http://www.astro.washington.edu/users/lynnej/opsim/enigma_1189_sqlite.db.gz > /dev/null 2>&1
 echo 'Downloads of opsim runs done'
 echo `ls `
-gunzip *.gz
+# gunzip *.gz
 echo 'unzip done'
 for dbfile in `ls *.db`; do 
     file=`basename $dbfile`; 
